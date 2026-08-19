f88665ebf3e7d5d285f8bb0e326a77863f165cc6
wassem <wasemayman60@gmail.com>
2026-08-19 19:26:47 +0300

    delete auth

--- projects/iunu-website/src/Routes/AppRoutes.jsx
+++ projects/iunu-website/src/Routes/AppRoutes.jsx
@@ -1,9 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home/Home";
-import Login from "../pages/Auth/Login";
-import Register from "../pages/Auth/Register";
-import ForgotPassword from "../pages/Auth/ForgotPassword";
 import Project from "../pages/Project/Project";
 import About from "../pages/About/About";
 import Contact from "../pages/Contact/Contact";
@@ -15,15 +12,7 @@ function AppRoutes() {
 
       <Route path="/" element={<Home />} />
 
-      <Route path="/login" element={<Login />} />
-
-      <Route path="/register" element={<Register />} />
-
-      <Route
-        path="/forgot-password"
-        element={<ForgotPassword />}
-      />
-
+    
       <Route
         path="/project"
         element={<Project />}
